3557f9d11563259beccd1a27df50e61bb4b83cfb
wicho <wichocanul07@gmail.com>
2023-10-07 23:50:54 -0600

    SAB: Create register user endpoint

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'user' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string',
+        ]);
+
+        if($validator->fails()) {
+            return response()->json([
+                'message' => 'error',
+                'details' => 'could not create user'
+            ], 400);
+        }
+
+        $user = User::create([
+            'user' => $request->user,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'success',
+            'data' => $user,
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
 }
--- routes/api_v1.php
+++ routes/api_v1.php
@@ -19,5 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('register-user', [AuthController::class, 'register']);
 
 Route::get('products', [ProductController::class, 'index']);
